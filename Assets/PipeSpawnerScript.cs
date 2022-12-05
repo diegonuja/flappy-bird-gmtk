8e24bbb97eba778db1c0b7d472328f5eaa707ee7
Diego Nuez <diego@bsit.com>
2022-12-05 09:52:48 +0100

    feat(pipeSpawner): add delay between spawns

--- Assets/PipeSpawnerScript.cs
+++ Assets/PipeSpawnerScript.cs
@@ -4,17 +4,38 @@ using UnityEngine;
 
 public class PipeSpawnerScript : MonoBehaviour
 {
+	// Amount of seconds between the spawn of 2 pipes
+	public float spawnerRate = 2;
+
+	public float heightOffest = 10;
+	private float timer = 0;
 
 	public GameObject pipe;
 	// Start is called before the first frame update
 	void Start()
 	{
-
+		spawnPipe();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		Instantiate(pipe, transform.position, transform.rotation);
+		if (timer >= spawnerRate)
+		{
+			spawnPipe();
+			timer = 0;
+		}
+		else
+		{
+			timer += Time.deltaTime;
+		}
+	}
+
+	private void spawnPipe()
+	{
+		float highestPoint = transform.position.y + heightOffest;
+		float lowestPoint = transform.position.y - heightOffest;
+
+		Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
 	}
 }
